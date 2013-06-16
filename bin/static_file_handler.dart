1ec4218b0008966bbabb73b5aa2513bf56e0eb56
Daniele Salatti <danielesalatti@gmail.com>
2013-06-16 22:03:32 +0200

    Adding help

--- bin/static_file_handler.dart
+++ bin/static_file_handler.dart
@@ -14,16 +14,12 @@ void printUsage(String script) {
   print("\t\t\t\tIf the same option is specified both as a command line argument");
   print("\t\t\t\tand into the config file, the config file will prevail");
   print("\t-d <root-path>\t\tSets the document root to <root-path>");
+  print("\t-h --help\t\tShows this help");
   print("\t-p <port>\t\tSets the port number to <port>");
 }
 
 void main() {
   Options options = new Options();
-
-  if (options.arguments.length < 2) {
-    printUsage(options.script);
-    exit(-1);
-  }
   
   var args = options.arguments;
   
@@ -47,6 +43,12 @@ void main() {
         
       case "-d":  // document root
         path = args[i+1];
+        break;
+        
+      case "-h":  // help
+      case "--help":
+        printUsage(options.script);
+        exit(0);
         break;
         
       case "-p":  // port
